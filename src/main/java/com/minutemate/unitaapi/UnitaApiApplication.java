d64cbe0bf72b0e97f1c551e4d611ae22137f7da3
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-02 17:59:43 +0900

    Initial commit

---
+++ src/main/java/com/minutemate/unitaapi/UnitaApiApplication.java
@@ -0,0 +1,13 @@
+package com.minutemate.unitaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minutemate/unitaapi/UnitaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minutemate.unitaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
