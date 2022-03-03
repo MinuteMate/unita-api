94601847471ee1155f2464fd54180443dbc53994
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-03 15:15:48 +0900

    [FIX] Service 와 Controller 의 중간계층으로 Authorizer 를 옮기고 요청에 대한 Frontier 메서드를 별도로 생성하도록 로직변경

--- src/main/java/com/minutemate/unitaapi/domain/account/controller/EmailAuthorizeController.java
+++ src/main/java/com/minutemate/unitaapi/domain/account/controller/EmailAuthorizeController.java
@@ -1,10 +1,14 @@
 package com.minutemate.unitaapi.domain.account.controller;
 
+import com.minutemate.unitaapi.domain.account.data.Authorizer;
 import com.minutemate.unitaapi.domain.account.data.identifier.number.AuthorizeNumber;
+import com.minutemate.unitaapi.domain.account.data.identifier.number.AuthorizeNumberGenerator;
 import com.minutemate.unitaapi.domain.account.data.token.AuthorizeToken;
 import com.minutemate.unitaapi.domain.account.email.target.AuthorizeEmail;
+import com.minutemate.unitaapi.domain.account.email.target.AuthorizeEmailGenerator;
 import com.minutemate.unitaapi.domain.account.email.token.EmailToken;
 import com.minutemate.unitaapi.domain.account.email.token.EmailTokenGenerator;
+import com.minutemate.unitaapi.domain.account.request.SendAuthorizeNumberRequest;
 import com.minutemate.unitaapi.domain.account.service.AuthorizeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,20 +17,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/authorize/email")
 @RequiredArgsConstructor
-public class EmailAuthorizeController implements AuthorizeController<AuthorizeNumber, AuthorizeEmail, EmailToken>{
+public class EmailAuthorizeController implements Authorizer<AuthorizeNumber, AuthorizeEmail, EmailToken> {
     private final AuthorizeService<AuthorizeEmail, AuthorizeNumber> authorizeService;
     private final EmailTokenGenerator emailTokenGenerator;
+    private final AuthorizeEmailGenerator authorizeEmailGenerator;
+    private final AuthorizeNumberGenerator authorizeNumberGenerator;
 
-    @Override @PostMapping
-    public ResponseEntity<?> sendAuthorizeNumber(@RequestBody AuthorizeEmail email) {
+    @PostMapping
+    public ResponseEntity<?> sendAuthorizeNumber(@RequestBody SendAuthorizeNumberRequest request) {
+        return sendAuthorizeNumber(authorizeEmailGenerator.of(request.email()));
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<?> getAuthorizeToken(@PathVariable Integer number) {
+        return getAuthorizeTokenByAuthorizeNumber(authorizeNumberGenerator.of(number));
+    }
+
+    @Override
+    public ResponseEntity<?> sendAuthorizeNumber(AuthorizeEmail email) {
         AuthorizeNumber number = authorizeService.generateIdentifier();
         email.sendIdentifier(number);
         authorizeService.addIdentifier(number, email);
         return ResponseEntity.ok().build();
     }
 
-    @Override @GetMapping
-    public ResponseEntity<AuthorizeToken> getAuthorizeTokenByAuthorizeNumber(@RequestBody AuthorizeNumber number) {
+    @Override
+    public ResponseEntity<AuthorizeToken> getAuthorizeTokenByAuthorizeNumber(AuthorizeNumber number) {
         AuthorizeEmail email = authorizeService.getEmailByIdentifier(number);
         EmailToken token = emailTokenGenerator.of(email);
         return ResponseEntity.ok(token);
--- src/main/java/com/minutemate/unitaapi/domain/account/data/Authorizer.java
+++ src/main/java/com/minutemate/unitaapi/domain/account/data/Authorizer.java
@@ -1,11 +1,11 @@
-package com.minutemate.unitaapi.domain.account.controller;
+package com.minutemate.unitaapi.domain.account.data;
 
 import com.minutemate.unitaapi.domain.account.data.identifier.AuthorizeIdentifier;
 import com.minutemate.unitaapi.domain.account.data.target.AuthorizeTarget;
 import com.minutemate.unitaapi.domain.account.data.token.AuthorizeToken;
 import org.springframework.http.ResponseEntity;
 
-public interface AuthorizeController<ID extends AuthorizeIdentifier, TARGET extends AuthorizeTarget<ID>, TOKEN extends AuthorizeToken> {
+public interface Authorizer<ID extends AuthorizeIdentifier, TARGET extends AuthorizeTarget<ID>, TOKEN extends AuthorizeToken> {
     //인증할 대상(이메일)을 받아서, 해당 대상에게 인증용 식별자를 송신한다
     //이후, 전송한 인증용식별자를 인증 대상과 매핑하여 저장한다.
     ResponseEntity<?> sendAuthorizeNumber(TARGET request);
