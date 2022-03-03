b22e32da61c6bf98a8f2ff44de51c6f0f12e0e9d
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-03 14:52:49 +0900

    [FIX] 자잘한 변경
    AuthorizeNumberEntity 에 lombok 을 통한 AllArgsConstructor 및 NoArgsConstructor 작성
    Controller 파라미터 이름 수정 및 요청정보관련 메타어노테이션 추가

--- src/main/java/com/minutemate/unitaapi/domain/account/controller/EmailAuthorizeController.java
+++ src/main/java/com/minutemate/unitaapi/domain/account/controller/EmailAuthorizeController.java
@@ -8,10 +8,7 @@ import com.minutemate.unitaapi.domain.account.email.token.EmailTokenGenerator;
 import com.minutemate.unitaapi.domain.account.service.AuthorizeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/authorize/email")
@@ -21,15 +18,15 @@ public class EmailAuthorizeController implements AuthorizeController<AuthorizeNu
     private final EmailTokenGenerator emailTokenGenerator;
 
     @Override @PostMapping
-    public ResponseEntity<?> sendAuthorizeNumber(AuthorizeEmail request) {
+    public ResponseEntity<?> sendAuthorizeNumber(@RequestBody AuthorizeEmail email) {
         AuthorizeNumber number = authorizeService.generateIdentifier();
-        request.sendIdentifier(number);
-        authorizeService.addIdentifier(number);
+        email.sendIdentifier(number);
+        authorizeService.addIdentifier(number, email);
         return ResponseEntity.ok().build();
     }
 
     @Override @GetMapping
-    public ResponseEntity<AuthorizeToken> getAuthorizeTokenByAuthorizeNumber(AuthorizeNumber number) {
+    public ResponseEntity<AuthorizeToken> getAuthorizeTokenByAuthorizeNumber(@RequestBody AuthorizeNumber number) {
         AuthorizeEmail email = authorizeService.getEmailByIdentifier(number);
         EmailToken token = emailTokenGenerator.of(email);
         return ResponseEntity.ok(token);
--- src/main/java/com/minutemate/unitaapi/domain/account/entity/AuthorizeNumberEntity.java
+++ src/main/java/com/minutemate/unitaapi/domain/account/entity/AuthorizeNumberEntity.java
@@ -1,7 +1,9 @@
 package com.minutemate.unitaapi.domain.account.entity;
 
 import com.minutemate.unitaapi.domain.account.email.target.AuthorizeEmail;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 
@@ -9,6 +11,8 @@ import javax.persistence.Id;
 
 @RedisHash("authorize-number")
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorizeNumberEntity {
     @Id
     private String authorizeNumber;
